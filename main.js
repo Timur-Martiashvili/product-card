def4116c97fad4ce983514c4598506d253412baf
Timur Martiashvili <timur.martiashvili@mail.ru>
2026-08-17 21:45:19 +0300

    Fix homework 6 comments

--- main.js
+++ main.js
@@ -7,9 +7,6 @@ const blueCOlorHash='#0000FF'
 changeColorAllCardButton.addEventListener('click',()=> {
   productCards.forEach((card) => card.style.backgroundColor = greenColorHash) 
 })
-
-
-
 // покраска первой карточки
 const firstProductCard = document.querySelector('.product-card');
 const changeColorFirstCardButton = document.querySelector ('#change-color-first-card');
@@ -25,27 +22,31 @@ function openGoogle () {
   const qustion = confirm ('Вы действительно хотите открыть Google?');
 
   if (qustion === true){
-    window.open ('https://google.com')
-  }
-    else {return;
+    window.open('https://google.com')
+  } else {
+    return
   }
 }
 
+// Заголовок
+const title = document.querySelector('h1');
+
 // Вывод в console log
+const outputLogButton = document.querySelector('#outputConsoleLog');
 
-const outputLogButton = document.querySelector ('#outputConsoleLog');
-outputLogButton.addEventListener('click',() => console.log(title.textContent)('ДЗ №4')) 
+outputLogButton.addEventListener('click', () => {
+  console.log(title.textContent, 'ДЗ №4');
+});
 
-function outputConsoleLog (message) {
-  alert (message)
-  console.log(message)
+function outputConsoleLog(message) {
+  alert(message);
+  console.log(message);
 }
 
 // Вывод в консоль заголовка
-const title = document.querySelector('h1');
-title.addEventListener('mouseover', () => {console.log ( title.textContent); })
+title.addEventListener('mouseover', () => {
+  console.log(title.textContent);
+});
 
-const button = document.querySelector ('#change-color-button');
-button.addEventListener ('click',() => {
-  button.classList.toggle ('change-color');
-})
+
+
