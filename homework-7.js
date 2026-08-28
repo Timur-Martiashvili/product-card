0ac92678aa849fb688beed2b767bf7e444d3baf9
Timur Martiashvili <timur.martiashvili@mail.ru>
2026-08-28 10:16:33 +0300

    Fix code formatting

--- homework-7.js
+++ homework-7.js
@@ -1,44 +1,42 @@
 console.log("Homework 7 работает");
-function showWeather(city,temperature){
-console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`)
+function showWeather(city, temperature) {
+  console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
 }
-showWeather("Москва",25)
+showWeather("Москва", 25);
+showWeather("Питер", 25);
 
 const speedOfLight = 299792458;
-function checkSpeed(speed){
-  if (speed>speedOfLight){
-    console.log("Сверхсветовая скорость")
+function checkSpeed(speed) {
+  if (speed > speedOfLight) {
+    console.log("Сверхсветовая скорость");
+  } else if (speed === speedOfLight) {
+    console.log("Скорость света");
+  } else {
+    console.log("Субсветовая скорость");
   }
-  else if(speed===speedOfLight){
-    console.log("Скорость света")
-  }
-  else {
-    console.log("Субсветовая скорость")
-  }
-  }
-  checkSpeed(399792458);
-  checkSpeed(299792458);
-  checkSpeed(199792458);
+}
+checkSpeed(399792458);
+checkSpeed(299792458);
+checkSpeed(199792458);
 
-const product="телефон";
-const productPrice=10000; 
-function buyProduct(budget){
-if (budget>productPrice){
-console.log(`${product} приобретен. Спасибо за покупку!`)
-}
-else {
-  console.log(`Вам не хватает ${productPrice-budget}, пополните баланс`)
-}
+const product = "телефон";
+const productPrice = 10000; 
+function buyProduct(budget) {
+  if (budget > productPrice) {
+    console.log(`${product} приобретен. Спасибо за покупку!`);
+  } else {
+    console.log(`Вам не хватает ${productPrice - budget}, пополните баланс`);
+  }
 }
 buyProduct(3090);
 
-function timePrayer(time){
-console.log(`${time} молитвы`)
+function timePrayer(time) {
+  console.log(`${time} молитвы`);
 }
 timePrayer("20:30");
 
-const name="Timur"; 
-const age=28; 
-const city="Moscow";
+const name = "Timur"; 
+const age = 28; 
+const city = "Moscow";
 
 
